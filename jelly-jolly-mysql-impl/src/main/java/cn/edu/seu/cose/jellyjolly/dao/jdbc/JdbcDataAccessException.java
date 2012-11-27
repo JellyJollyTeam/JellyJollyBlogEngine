3d8525753d6de73147e5e99e2483360fba0076cf
Wenhao Ji <predator.ray@gmail.com>
2012-11-27 21:08:46 +0800

    重构持久层实现包名；删除原有的依赖实现类

--- jelly-jolly-mysql-impl/src/main/java/cn/edu/seu/cose/jellyjolly/dao/jdbc/JdbcDataAccessException.java
+++ jelly-jolly-mysql-impl/src/main/java/cn/edu/seu/cose/jellyjolly/dao/jdbc/JdbcDataAccessException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 rAy <predator.ray@gmail.com>
+ * Copyright (C) 2012 Colleage of Software Engineering, Southeast University
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,19 +14,32 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package cn.edu.seu.cose.jellyjolly.dao.jdbc;
 
-package cn.edu.seu.cose.jellyjolly.model.dao.jdbc.mysql;
-
-import cn.edu.seu.cose.jellyjolly.model.dao.jdbc.MysqlDataAccessFactory;
+import cn.edu.seu.cose.jellyjolly.dao.DataAccessException;
+import java.sql.SQLException;
 
 /**
  *
  * @author rAy <predator.ray@gmail.com>
  */
-public class MysqlDataAccessFactoryImpl extends MysqlDataAccessFactory {
-    
-    public MysqlDataAccessFactoryImpl() {
-        super(new MysqlConnectionFactory());
+public class JdbcDataAccessException extends DataAccessException {
+
+    private static final String DEFAULT_HINT = "jdbc data access exception";
+
+    public JdbcDataAccessException() {
+        super(DEFAULT_HINT);
     }
 
+    public JdbcDataAccessException(String hint) {
+        super(hint);
+    }
+
+    public JdbcDataAccessException(SQLException e) {
+        super(e);
+    }
+
+    public JdbcDataAccessException(String hint, SQLException e) {
+        super(hint, e);
+    }
 }
